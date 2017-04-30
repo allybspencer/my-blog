7f4fbec17803f8f50198855c2c6cf04ba2873186
Ally Spencer <allybspencer@gmail.com>
2017-04-30 02:59:54 +0000

    I GOT COMMENTS WERKING!

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,16 @@ class Post(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.title
+        
+class HitCount(models.Model):
+    hits = models.IntegerField()
+    
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    #comment_day = models.DateTimeField('date posted', blank=True, null=True)
+    
+    def __str__(self):
+        return self.text
+    
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,21 @@
 from django.http import Http404
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.template import loader
 
-from .models import Post
+from .models import Post, Comment
+from .forms import CommentForm
 
 from django.http import HttpResponse
 
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('detail', pk=comment.post.pk)
+
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
 
 def index(request):
     latest_post_list = Post.objects.order_by('-pub_date')[:5]
@@ -14,7 +24,21 @@ def index(request):
         'latest_post_list': latest_post_list,
     }
     return HttpResponse(template.render(context, request))
+ 
     
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, 'blog/detail.html', {'post': post})
+    
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('details', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
